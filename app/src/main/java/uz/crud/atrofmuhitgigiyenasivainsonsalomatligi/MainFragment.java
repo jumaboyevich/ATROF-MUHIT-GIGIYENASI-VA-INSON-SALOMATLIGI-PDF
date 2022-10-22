c67ff78ee4b9d6ddc1818145cd4b6f43ed028b4c
ilhomjon <catchy977@gmail.com>
2022-10-22 08:43:07 +0500

    After release

--- app/src/main/java/uz/crud/atrofmuhitgigiyenasivainsonsalomatligi/MainFragment.java
+++ app/src/main/java/uz/crud/atrofmuhitgigiyenasivainsonsalomatligi/MainFragment.java
@@ -47,10 +47,10 @@ public class MainFragment extends Fragment implements ThemeAdapter.ItemClickList
         listNoteList.add(new ListNote("KIRISH", themeNoteList));
         themeNoteList=new ArrayList<>();
 
-        themeNoteList.add(new ThemeNote("1.1.1. Ekotizim va salomatlik", new int[]{4,5,6}));
+        themeNoteList.add(new ThemeNote("1.1. Ekotizim va salomatlik", new int[]{4,5,6}));
 //        themeNoteList.add(new ThemeNote("1.1.2. Atrof-muhit tushunchasi", new int[]{5}));
 //        themeNoteList.add(new ThemeNote("1.1.3. Atrof muhit omillari va tizimi", new int[]{5,6}));
-        themeNoteList.add(new ThemeNote("1.2.1. Atrof - muhitga moslashish va uni insonga ta’siri", new int[]{6,7,8,9}));
+        themeNoteList.add(new ThemeNote("1.2. Atrof - muhitga moslashish va uni insonga ta’siri", new int[]{6,7,8,9}));
 //        themeNoteList.add(new ThemeNote("1.2.2. Atrof - muhit omillarini tasniflash", new int[]{6,8}));
 //        themeNoteList.add(new ThemeNote("1.2.3. Gomeostaz, moslashish, tozalashning ta’rifi va tushunchalari", new int[]{8,9}));
         themeNoteList.add(new ThemeNote("1.3. Atrof – muhit salomatligi tarixi va ifloslanish halokatlari", new int[]{9,10,11,12,13,14,15,16}));
@@ -61,11 +61,25 @@ public class MainFragment extends Fragment implements ThemeAdapter.ItemClickList
 
         listNoteList.add(new ListNote("I BOB. ATROF MUHIT VA INSON SALOMATLIGI", themeNoteList));
         themeNoteList=new ArrayList<>();
-        themeNoteList.add(new ThemeNote("1.7. To’xtatgichlar va tormozlar", new int[]{65,66,67,68,69,70,71}));
-        themeNoteList.add(new ThemeNote("1.8. Tormozlar", new int[]{68,69,70,71,}));
-        themeNoteList.add(new ThemeNote("1.9. Qurilish mashinalari haqida umumiy ma’lumot", new int[]{72,73,74,75}));
+        themeNoteList.add(new ThemeNote("2.2. Xavfli kimyoviy moddalarni nazorat qilish tizimi", new int[]{17,18,19,20,21,22,23,24,25,26,27}));
+
         listNoteList.add(new ListNote("II BOB. ATROF MUHIT OMILLARINI ME’YORLASHTIRISH", themeNoteList));
-//        themeNoteList.add(new ThemeNote("", new int[]{1,2,3}));
+        themeNoteList=new ArrayList<>();
+        themeNoteList.add(new ThemeNote("3.1. Atmosfera havosining sifatini tekshirish va baholash", new int[]{29,30,31,32,33,34,35,36,37,38,39}));
+        themeNoteList.add(new ThemeNote("3.2. Transchegaraviy va global atrof-muhit ifloslanishi.", new int[]{39,40,41,42,43,44,45,46}));
+        listNoteList.add(new ListNote("III BOB. ATMOSFERA HAVOSINING SIFATINI EKSPERIMENTAL TEKSHIRISHLAR", themeNoteList));
+        themeNoteList=new ArrayList<>();
+        themeNoteList.add(new ThemeNote("4.1. Atmosferaning ifloslanishi", new int[]{46,47,48,49,50,51,52,53,54}));
+        themeNoteList.add(new ThemeNote("4.2. Xorijda ifloslanish bilan bog‘liq halokatlar", new int[]{54,55,56,57,58,59,60,61}));
+        themeNoteList.add(new ThemeNote("4.3. Koreyadagi ifloslanish bilan bog‘liq halokatlar", new int[]{61,62,63,64,65,66,67,68,69}));
+        listNoteList.add(new ListNote("IV BOB. CHIQINDILAR", themeNoteList));
+        themeNoteList=new ArrayList<>();
+        themeNoteList.add(new ThemeNote("5.1. Suvning sirkulyatsiyasi", new int[]{69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90}));
+
+        listNoteList.add(new ListNote("V BOB. GIDROSFERA MENEJMENTI", themeNoteList));
+
+
+        //        themeNoteList.add(new ThemeNote("", new int[]{1,2,3}));
 
 
 
